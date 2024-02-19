44eeffc1273fd03dc853518f1c964c9fb98beb45
S-exypig <1321987303@qq.com>
2024-02-20 00:39:50 +0800

    客户端新增模式选择和改名功能 2/20

--- client.go
+++ client.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
+
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "localhost", "设置客户端所要连接的服务器的ip")
+	flag.IntVar(&serverPort, "port", 8000, "设置客户端所要连接的服务器的端口")
+}
+
+func main() {
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
+	if client == nil {
+		fmt.Println(">>>>> 连接服务器失败...")
+		return
+	}
+	fmt.Println(">>>>> 连接服务器成功~~")
+	go client.DealResponse()
+	client.Run()
+}
 
 type client struct {
 	serverIp   string
 	serverPort int
 	name       string
 	conn       net.Conn
+	mode       int
 }
 
 func NewClient(ip string, port int) *client {
@@ -31,12 +56,61 @@ func NewClient(ip string, port int) *client {
 	return client
 }
 
-func main() {
-	client := NewClient("localhost", 8000)
-	if client == nil {
-		fmt.Println(">>>>> 连接服务器失败...")
-		return
+func (c *client) Menu() bool {
+	fmt.Println("0.退出")
+	fmt.Println("1.私聊模式")
+	fmt.Println("2.公聊模式")
+	fmt.Println("3.修改用户名")
+	fmt.Println("4.查看在线用户")
+	var mode int
+	fmt.Scanln(&mode)
+	if mode < 0 || mode > 4 {
+		fmt.Println("输入有误!")
+		return false
 	}
-	fmt.Println(">>>>> 连接服务器成功~~")
-	select {}
+	c.mode = mode
+	return true
+}
+
+func (c *client) Run() {
+LOOP:
+	for {
+		for c.Menu() != true {
+		}
+		switch c.mode {
+		case 0:
+			fmt.Println("退出!")
+			break LOOP
+		case 1:
+			fmt.Println("私聊模式...")
+		case 2:
+			fmt.Println("公聊模式...")
+		case 3:
+			if c.UpdateName() {
+				fmt.Println("改名成功!")
+			} else {
+				fmt.Println("改名未成功!")
+			}
+		case 4:
+			fmt.Println("查看在线用户...")
+		}
+	}
+}
+
+func (c *client) UpdateName() bool {
+	fmt.Println(">>>>>请输入用户名:")
+	fmt.Scanln(&c.name)
+	sendMsg := fmt.Sprintf("rename|%v\n", c.name)
+	_, err := c.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Printf("conn.Write Error:%v\n", err)
+		return false
+	}
+	return true
+}
+
+// 处理S端回应的信息，直接打印到标准输出
+func (c *client) DealResponse() {
+	// 一旦客户端的conn收到数据，直接copy到标准输出流
+	io.Copy(os.Stdout, c.conn)
 }
