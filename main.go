d1503dd06911a73b3e2a30f55fe8b59986905a1b
S-exypig <1321987303@qq.com>
2024-02-20 01:53:10 +0800

    完善服务端可指定ip和端口号功能 2/20

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import "flag"
+
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "localhost", "指定服务端的ip地址")
+	flag.IntVar(&serverPort, "port", 8000, "指定服务端的端口号")
+}
+
 func main() {
-	myServer := NewServer("localhost", 8000)
+	flag.Parse()
+	myServer := NewServer(serverIp, serverPort)
 	myServer.Start()
 }
